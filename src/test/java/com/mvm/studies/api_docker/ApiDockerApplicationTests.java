f5a531d9ae5c31ae00579571c2588b53dfab3677
Marcondes <mvm.sp11@gmail.com>
2026-05-30 17:36:52 -0300

    estrutura inicial

---
+++ src/test/java/com/mvm/studies/api_docker/ApiDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvm.studies.api_docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
